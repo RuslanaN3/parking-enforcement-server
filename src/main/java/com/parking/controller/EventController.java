4e19e72adeb78ff975b966248f65137d6040f589
ruslana <ruslanka5555gmail.com>
2022-11-27 00:57:47 +0200

    1311 RouteCycle updates, event processing update, transient state

--- src/main/java/com/parking/controller/EventController.java
+++ src/main/java/com/parking/controller/EventController.java
@@ -2,8 +2,6 @@ package com.parking.controller;
 
 import com.parking.dto.EventDto;
 import com.parking.model.Event;
-import com.parking.model.ParkingArea;
-import com.parking.repository.ParkingAreaRepository;
 import com.parking.service.EventService;
 import java.util.Comparator;
 import java.util.List;
@@ -16,20 +14,21 @@ import org.springframework.web.bind.annotation.*;
 public class EventController {
 
     private EventService eventService;
-    private ParkingAreaRepository parkingAreaRepository;
 
-    public EventController(EventService eventService,
-                           ParkingAreaRepository parkingAreaRepository) {
+    public EventController(EventService eventService) {
         this.eventService = eventService;
-        this.parkingAreaRepository = parkingAreaRepository;
     }
 
     @PostMapping
-    public EventDto handleEvent(@Valid @RequestBody EventDto eventDto, BindingResult bindingResult) {
+    public void handleEvent(@Valid @RequestBody EventDto eventDto, BindingResult bindingResult) {
         if (!bindingResult.hasErrors()) {
             eventService.handleEvent(eventDto);
         }
-        return eventDto;
+    }
+
+    @GetMapping
+    public List<Event> getEvents() {
+        return eventService.getEvents();
     }
 
     @PostMapping("multiple")
@@ -41,29 +40,24 @@ public class EventController {
         return eventDtos.get(0);
     }
 
-    @GetMapping
-    public List<Event> getEvents() {
-        return eventService.getEvents();
-    }
-
-    @GetMapping("/{lat}/{lon}")
-    public ParkingArea getPointEntity(@PathVariable Double lat, @PathVariable Double lon) {
-        //List<ParkingArea> pointEntities = pointEntityRepository.findAll();
-        //Coordinate[] coordinates = new Coordinate[5];
-        //coordinates[0] = new Coordinate(1, 2); // Starting point
-        //coordinates[1] = new Coordinate(2, 20);
-        //coordinates[2] = new Coordinate(20, 36);
-        //coordinates[3] = new Coordinate(36, 100);
-        //coordinates[4] = new Coordinate(1, 2); // Ending point
-        //LinearRing linear = new GeometryFactory().createLinearRing(coordinates);
-        //Polygon poly = new Polygon(linear, null, new GeometryFactory());
-        //ParkingArea parkingPolygon = new ParkingArea();
-        //parkingPolygon.setId(5);
-        //parkingPolygon.setMpolygon(poly);
-        //pointEntityRepository.save(parkingPolygon);
-
-        //Point p = new GeometryFactory().createPoint(new Coordinate(lat, lon));
-        return parkingAreaRepository.findParkingAreaByPoint(lat, lon);
-    }
+    //@GetMapping("/{lat}/{lon}")
+    //public ParkingArea getPointEntity(@PathVariable Double lat, @PathVariable Double lon) {
+    //    //List<ParkingArea> pointEntities = pointEntityRepository.findAll();
+    //    //Coordinate[] coordinates = new Coordinate[5];
+    //    //coordinates[0] = new Coordinate(1, 2); // Starting point
+    //    //coordinates[1] = new Coordinate(2, 20);
+    //    //coordinates[2] = new Coordinate(20, 36);
+    //    //coordinates[3] = new Coordinate(36, 100);
+    //    //coordinates[4] = new Coordinate(1, 2); // Ending point
+    //    //LinearRing linear = new GeometryFactory().createLinearRing(coordinates);
+    //    //Polygon poly = new Polygon(linear, null, new GeometryFactory());
+    //    //ParkingArea parkingPolygon = new ParkingArea();
+    //    //parkingPolygon.setId(5);
+    //    //parkingPolygon.setMpolygon(poly);
+    //    //pointEntityRepository.save(parkingPolygon);
+    //
+    //    //Point p = new GeometryFactory().createPoint(new Coordinate(lat, lon));
+    //    return parkingAreaRepository.findParkingAreaByPoint(lat, lon);
+    //}
 
 }
